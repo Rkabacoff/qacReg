e47e62be9fc1ee45a604c030978ec9b59ef99d3f
Robert Kabacoff <rkabacoff@wesleyan.edu>
2021-11-07 14:35:11 -0500

    initial commit

---
+++ R/mreg.R
@@ -0,0 +1,22 @@
+#'@title Multiple Regression
+#'
+#'@description Fit a linear model of the form
+#'  \eqn{y = \beta0 + \beta1*X1 + \beta2*X2 + ...+ \betak*Xk}.
+#'
+#'@param formula an object of class formula.
+#'@param data a data frame.
+#'@param ... arguments passed to the \code{lm} function.
+#'
+#'@export
+#'@details
+#'\code{mreg} is a wrapper for the \code{\link{lm}} function.
+#'
+#'@return an object of type \code{c("mreg", "lm")}
+#'
+#'@examples
+#'fit <- mreg(mpg ~ hp + wt + am, mtcars)
+mreg <- function(formula, data, ...){
+  fit <- lm(formula, data, ...)
+  class(fit) <- c("mreg", "lm")
+  return(fit)
+}
