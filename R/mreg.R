d401c329d78e242830a8e985a00664b19a6dbb47
Robert Kabacoff <rkabacoff@wesleyan.edu>
2021-11-28 20:36:21 -0500

    summary, plot, and diagnostics added

--- R/mreg.R
+++ R/mreg.R
@@ -20,5 +20,6 @@ mreg <- function(formula, data){
   fit$call <- str2lang(paste("lm(formula=", deparse(substitute(formula)),
                              ", data=", deparse(substitute(data)), ")"))
   class(fit) <- c("mreg", "lm")
+  return(fit)
 }
 
